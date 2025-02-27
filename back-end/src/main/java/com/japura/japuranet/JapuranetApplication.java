f07c559cc3472d407c4bc3e7add5e945c2aba6ab
LahiruCW <as2021356@sci.sjp.ac.lk>
2025-02-27 14:11:48 +0530

    Initialize the Back end

---
+++ back-end/src/main/java/com/japura/japuranet/JapuranetApplication.java
@@ -0,0 +1,13 @@
+package com.japura.japuranet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapuranetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JapuranetApplication.class, args);
+	}
+
+}
